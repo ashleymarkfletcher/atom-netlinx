21c571e128451f9a6e787c0e9824dc0c4c1a2422
ashleymarkfletcher <ashleymarkfletcher@gmail.com>
2017-06-08 21:40:22 +0100

    starter project added

---
+++ spec/atom-netlinx-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtomNetlinxView from '../lib/atom-netlinx-view';
+
+describe('AtomNetlinxView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
